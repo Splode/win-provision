df3e432deca8c6dc78e2c1a1fb12b96006bb92a8
Christopher Murphy <flyweight@pm.me>
2020-03-06 08:22:45 -0700

    feat: update init, add personal

--- init.ps1
+++ init.ps1
@@ -1,17 +1,20 @@
 Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
-set-executionpolicy unrestricted -s cu
+Set-ExecutionPolicy unrestricted -s cu
 
 # Utils
-scoop install 7zip curl sudo git openssh coreutils ripgrep sed less bat wget jq
+scoop install 7zip curl sudo git openssh coreutils ripgrep sed less bat wget jq ffmpeg micro vagrant
 
 # Add extras
 scoop bucket add extras
 
 # Applications
-scoop install cmder firefox heidisql vscode vcredist2019
+scoop install cmder firefox heidisql vscode vcredist2019 portable-virtualbox
 
 # Languages
-scoop install nodejs-lts python rust
+scoop install nodejs-lts php
+
+# PHP Composer
+scoop install composer
 
 # Keep shell open
 Read-Host "Press Enter to exit..."
---
+++ personal/install.ps1
@@ -0,0 +1,1 @@
+scoop install anki go hugo pandoc python qbittorrent restic rust task
